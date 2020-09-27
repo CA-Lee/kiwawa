07eec811ab2448d8c1653e2964cb44a4bbc9597d
CA-Lee <email@calee.tw>
2020-09-27 18:07:56 +0800

    add feature: discord 2 line

--- app2.py
+++ app2.py
@@ -1,12 +1,28 @@
 import discord
 from discord.ext import commands
 
+from lotify.client import Client
+
 import os
+import json
 
 bot = commands.Bot(command_prefix='>')
+lotify_token = os.environ['LOTIFY_TOKEN']
+lotify = Client()
 
 @bot.command()
 async def ping(ctx):
+    lotify.send_message(
+        access_token = lotify_token,
+        message = 'pong'
+    )
     await ctx.send('pong')
+
+@bot.listen()
+async def on_message(message):
+    lotify.send_message(
+        access_token = lotify_token,
+        message = json.dumps(message)
+    )
 
 bot.run(os.environ['DISCORDBOT_TOKEN'])
