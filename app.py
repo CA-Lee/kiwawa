7111fc3a563b78d9786d50d44844fc60f6f23363
CA-Lee <email@calee.tw>
2020-09-27 15:09:00 +0800

    fix bug

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     request_body = {"content":event}
-    requests.post(url=discord_webhook,body=json.loads(request_body))
+    requests.post(url=discord_webhook,body=request_body)
     # line_bot_api.reply_message(
     #     event.reply_token,
     #     TextSendMessage(text=event.message.text)
